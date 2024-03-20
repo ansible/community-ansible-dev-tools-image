0a2b59f0a22a6570808f155db5b4fc824fde1076
NilashishC <nilashishchakraborty8@gmail.com>
2024-03-20 20:58:17 +0530

    Updates
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,17 @@
 echo "::group::Starting tests"
 
-echo "::debug::switching to tests directory"
+set -ex
+
+echo "::notice::switching to tests directory"
 cd /home/podman/tests
 
-echo "::debug::validate that adt is installed"
-adt --version
+echo "::notice::validate that adt is installed"
+adt1 --version
 
-echo "::debug::validate that container in container works"
+echo "::notice::validate that container in container works"
 podman run hello
 
-echo "::debug::validate that ansible-navigator works inside the container with EE"
+echo "::notice::validate that ansible-navigator works inside the container with EE"
 ansible-navigator run site.yml --mode stdout
 
 echo "::endgroup::"
